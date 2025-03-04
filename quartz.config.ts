b268a596ed73f594e8d3563cef079a33bb3b4c2b
Fakhruddin Saupe <30628450+Fakhruddin90@users.noreply.github.com>
2025-03-04 23:46:19 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "din-dev.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
